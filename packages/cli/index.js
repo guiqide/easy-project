b10a2fca7036039a7d2e063edae0e5084f9bb06c
kevingui <kevingui@tencent.com>
2021-01-11 17:33:21 +0800

    feat: 使用lerna给每个包增加lodash

---
+++ packages/cli/index.js
@@ -0,0 +1,3 @@
+"use strict";
+
+module.exports = require("./dist/cli.js");
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -9,66 +9,66 @@ interface IOptions {
   isExistProject: boolean;
 }
 
-export class ImportCore {
-  static pluginList: IPluginData[];
-  pluginsNames: string[];
-  options: IOptions;
-  constructor() {
-    this.start();
-  }
+const pluginList: IPluginData[] = [];
 
-  async start(): Promise<void> {
-    ImportCore.pluginList.forEach((pluginData) => {
-      this.pluginsNames.push(pluginData.name);
-    });
+const pluginsNames: string[] = [];
 
-    await this.startInteract();
-    const dir = await this.createDir();
-    if (!this.options.isExistProject) {
-      await this.initProject();
-    }
-    await this.copyFiles(dir);
-  }
+let options: IOptions;
 
-  // async startInteract(): IOptions {}
+// function startInteract(): IOptions {}
 
-  async createDir(): Promise<string> {
-    const dir = path.resolve(__dirname, this.options.projectName);
+async function createDir(): Promise<string> {
+  const dir = path.resolve(__dirname, options.projectName);
 
-    if (this.options.isExistProject) {
-      return dir;
-    }
-
-    fs.mkdirSync(dir, {
-      mode: "755",
-    });
+  if (options.isExistProject) {
     return dir;
   }
 
-  async initProject() {
-    if (!shell.which("npm")) {
-      shell.echo("Sorry, this script requires npm");
-      shell.exit(1);
-    }
-    if (shell.exec("npm init").code !== 0) {
-      shell.echo("npm initial error, please try again");
-      shell.exit(1);
-    }
-  }
+  fs.mkdirSync(dir, {
+    mode: "755",
+  });
+  return dir;
+}
 
-  private copyFiles(projectDIR: string) {
-    ImportCore.pluginList.forEach((pluginData) => {
-      pluginData.files.forEach((file) => {
-        const name = file;
-        fs.copyFile(file, projectDIR, {
-          mode: "755",
-        });
-      });
-    });
+async function initProject() {
+  if (!shell.which("npm")) {
+    shell.echo("Sorry, this script requires npm");
+    shell.exit(1);
   }
-
-  // private execCommands() {}
-  use(plugin: IUse): void {
-    ImportCore.pluginList.push(plugin.install());
+  if (shell.exec("npm init").code !== 0) {
+    shell.echo("npm initial error, please try again");
+    shell.exit(1);
   }
 }
+
+function copyFiles(projectDIR: string) {
+  pluginList.forEach((pluginData) => {
+    pluginData.files.forEach((file) => {
+      const name = file;
+      fs.copyFile(file, projectDIR, {
+        mode: "755",
+      });
+    });
+  });
+}
+
+function use(plugin: IUse): void {
+  pluginList.push(plugin.install());
+}
+
+export async function CLI() {
+  pluginList.forEach((pluginData: IPluginData) => {
+    pluginsNames.push(pluginData.name);
+  });
+
+  await startInteract();
+  const dir = await createDir();
+
+  if (!options.isExistProject) {
+    await initProject();
+  }
+
+  await copyFiles(dir);
+}
+
+CLI();
